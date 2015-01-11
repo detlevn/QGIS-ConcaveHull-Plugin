7e91ae98eb3e0609be09e1eb262fee39fb191957
detlevn <dneumann@geospatial-services.de>
2015-01-11 11:02:46 +0100

    Added translation file DE

---
+++ i18n/concavehull_de.ts
@@ -0,0 +1,112 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="DE">
+<context>
+    <name>ConcaveHull</name>
+    <message>
+        <location filename="../concavehull.py" line="480"/>
+        <source>No shapefile name specified</source>
+        <translation>Keine Shapedatei angegeben</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="282"/>
+        <source>ConcaveHull</source>
+        <translation>ConcaveHull</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="32"/>
+        <source>Specify the number of nearest neighbors to start with</source>
+        <translation>Mindestgröße der Nachbarschaft</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="35"/>
+        <source>Number of neighbors</source>
+        <translation>Anzahl der Nachbarpunkte</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="88"/>
+        <source>Check and specify a neighborhood size to cluster the data prior constructing a hull</source>
+        <translation>Mindestgröße der Cluster angeben</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="91"/>
+        <source>Find SNN clusters</source>
+        <translation>SNN Cluster bilden</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="136"/>
+        <source>Neighborhood size</source>
+        <translation>Mindestgröße der Cluster</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="217"/>
+        <source>Input layers</source>
+        <translation>Eingabelayer</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="226"/>
+        <source>Select one or more input layers</source>
+        <translation>Selektiere einen oder mehrere Layer</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="236"/>
+        <source>Use only selected features on input layers</source>
+        <translation>Nur selektierte Objekte verwenden</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="239"/>
+        <source>Use selected features only</source>
+        <translation>Nur selektierte Objekte verwenden</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="249"/>
+        <source>Output layer</source>
+        <translation>Ausgabelayer</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="255"/>
+        <source>New memory layer</source>
+        <translation>Neuen Memorylayer anlegen</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="279"/>
+        <source>Type in the name of a new memory layer</source>
+        <translation>Gebe den Namen des neuen Memorylayer an</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="291"/>
+        <source>Existing layer</source>
+        <translation>Bestehenden Layer verwenden</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="312"/>
+        <source>Choose an existing polygon layer from the list</source>
+        <translation>Wähle einen bestehenden Polygonlayer aus der Liste</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="327"/>
+        <source>New shapefile</source>
+        <translation>Neue Shapedatei erstellen</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="364"/>
+        <source>Browse for the location of a new or existing Shape file</source>
+        <translation>Neue Shapedatei angeben oder eine bestehende Datei auswählen</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="367"/>
+        <source>Browse ...</source>
+        <translation>Durchsuchen</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="376"/>
+        <source>Add the new layer to the map</source>
+        <translation>Ergebnis der Karte hinzufügen</translation>
+    </message>
+    <message>
+        <location filename="../ui_concavehull.ui" line="379"/>
+        <source>Add result to map</source>
+        <translation>Ergebnis der Karte hinzufügen</translation>
+    </message>
+</context>
+</TS>
